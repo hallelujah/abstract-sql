fc5db2a3acdb44639814e69a80a9dd9e60ef8135
Ramihajamalala Hery <hery@rails-royce.org>
2011-08-01 16:44:10 +0200

    Create the gem via bundler

---
+++ lib/abstract-sql.rb
@@ -0,0 +1,7 @@
+require "abstract-sql/version"
+
+module Abstract
+  module Sql
+    # Your code goes here...
+  end
+end
---
+++ lib/abstract-sql/version.rb
@@ -0,0 +1,5 @@
+module Abstract
+  module Sql
+    VERSION = "0.0.1"
+  end
+end
